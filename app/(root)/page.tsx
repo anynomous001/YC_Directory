b576c377309f7b934d62262767fc55d2b29c5a45
PRITAM CHAKROBORTY <chakrobortypritam1@gmail.com>
2024-10-28 11:46:25 +0530

    heading and sub heading added to haer section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,13 @@
 
 export default function Home() {
   return (
-    <div className="heading">HOME</div>
+    <section className="pink_container">
+
+      <h1 className="heading">Pitch Your Startup, <br /> connect with Entrepreneurs.</h1>
+      <p className="sub-heading !max-w-3xl">
+        Submit Ideas , Vote on Pitches , and get Noticed in Virtual Competitions.
+      </p>
+    </section>
+
   );
 }
