e656e59387422f4c828679d2ca5f4b30270409fc
PRITAM CHAKROBORTY <chakrobortypritam1@gmail.com>
2024-10-28 09:49:09 +0530

    navbar added

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,62 @@
+import { auth, signIn, signOut } from "@/auth"
+import Image from "next/image"
+import Link from "next/link"
+
+const Navbar = async () => {
+
+    const session = await auth()
+
+
+    return (
+        <header className="bg-slate-50 py-6 px-5 text-black">
+            <nav className="flex justify-between items-center ">
+
+                <Link href={'/'}>
+                    <Image src={"/logo.png"} alt="Logo" width={144} height={30} />
+                </Link>
+
+
+
+                <div className="flex items-center gap-6">
+                    {
+                        session && session?.user ? (
+                            <>
+                                <Link href={'/startup/create'}>
+                                    <span>Create</span>
+                                </Link>
+                                <form action={async () => {
+                                    "use server"
+                                    await signOut()
+                                }}>
+
+
+                                    <button type="submit">
+                                        <span>Log Out</span>
+                                    </button>
+                                </form>
+
+
+                                <Link href={`/user/${session?.user?.id}`}>
+                                    <span>{session?.user?.name}</span>
+                                </Link>
+                            </>
+
+                        ) : (
+
+                            <form action={async () => {
+                                "use server"
+
+                                await signIn("github")
+                            }}>
+                                <button type="submit">
+                                    <span>Log In</span>
+                                </button>
+                            </form>
+                        )}
+                </div>
+            </nav>
+        </header >
+    )
+}
+
+export default Navbar
